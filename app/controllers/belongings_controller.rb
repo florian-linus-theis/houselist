6143c3aa60cbbd487c0b9565e57bb8bcec7d252c
Filipa Vukovic <hrgafilipa@gmail.com>
2023-08-29 14:31:40 +0200

    stared

--- app/controllers/belongings_controller.rb
+++ app/controllers/belongings_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class BelongingsController < ApplicationController
+
+  def show
+    @beloninging = Belonging.find(params[:id])
+  end
+
+  def create
+    
+  end
 end
