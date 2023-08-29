8f5bd62ffc43bca4ad0756582af4536807c16d4f
Isabel Dockemeyer <isabel.dockemeyer@web.de>
2023-08-29 12:08:14 +0200

    Routes for Houselist

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'pages#home'
+  resources :flats, only: [:index, :show, :create, :update, :destroy] do
+    resources :belongings, only: [:index, :show, :create, :update, :destroy ]
+  end
+  resources :todos, only: [:show, :create, :destroy]
+  resources :users, only: [:show, :edit, :update] do
+    resources :notifications, only: [:show, :index, :create]
+  end
 end
