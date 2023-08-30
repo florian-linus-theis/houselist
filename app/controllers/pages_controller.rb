ca76fc94eb133569e746bad3413b278454f2fed1
Florian Theis <florian.theis@ib-theis.de>
2023-08-30 09:36:16 +0200

    user is redirected to flats_path if user signed in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,8 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
-  def home; end
+  def home
+    # If the user is signed in, he/she will always be forwarded to flats page
+    redirect_to flats_path if user_signed_in?
+  end
 end
