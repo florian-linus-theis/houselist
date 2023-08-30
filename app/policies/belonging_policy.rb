4aeebad09b6d93f90638e39c2f68cea38a7028be
Florian Theis <florian.theis@ib-theis.de>
2023-08-30 16:11:10 +0200

    added new policy belongings

--- app/policies/belonging_policy.rb
+++ app/policies/belonging_policy.rb
@@ -1,13 +1,18 @@
 class BelongingPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-     def resolve
-       scope.all
-     end
+    def resolve
+      scope.all
+    end
   end
 
   def show?
-    true
+    if user.landlord?
+      record.flat.user = user # landlords can only see their belongings
+    elsif user.tenant?
+      # Tenants can only see belongings of flats where they are added
+      record.flat.tenants.any? { |tenant| tenant.user == user }
+    end
   end
 
   def create?
