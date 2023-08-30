3ad46c51ffd85053b3a9f35fbfbe01d94c6278b9
Florian Theis <florian.theis@ib-theis.de>
2023-08-30 18:05:56 +0200

    Added notifications functionality

---
+++ app/javascript/controllers/flat_notifications_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="flat-notifications"
+export default class extends Controller {
+  static targets = ["message"]
+
+  connect() {
+    console.log('hello world')
+  }
+
+  toggle() {
+    console.log('hello from toggle')
+    const elem = this.messageTarget
+    if(elem.classList.contains("closed")){
+      elem.classList.add("showing");
+      elem.classList.remove("closed");
+    }else if(elem.classList.contains("showing")){
+      elem.classList.add("closed");
+      elem.classList.remove("showing");
+    }else{
+      elem.classList.toggle("closed");
+    }
+  }
+}
