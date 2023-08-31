1fe27ea722659147d3d6dff30af45019cbc7b6f7
James Hibbeard <jhibbeard@gmail.com>
2023-08-31 15:53:25 +0200

    notifications model spec updated

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -16,5 +16,10 @@ RSpec.describe Notification, type: :model do
   describe 'associations' do
     it { should belong_to(:user) }
     it { should belong_to(:belonging) }
+    it { should belong_to(:todo) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :description }
   end
 end
