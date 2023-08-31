30693f11b6e8f19e21ec179601d64178721abf20
James Hibbeard <jhibbeard@gmail.com>
2023-08-31 15:52:05 +0200

    Updated the belongin and category test files

--- spec/models/belonging_spec.rb
+++ spec/models/belonging_spec.rb
@@ -18,7 +18,16 @@ RSpec.describe Belonging, type: :model do
   describe 'associations' do
     it { should belong_to(:flat) }
     it { should belong_to(:category) }
-    it { should have_many(:notifications) }
-    it { should have_many(:todos) }
+    it { should have_many(:notifications).dependent(:destroy) }
+    it { should have_many(:todos).dependent(:destroy) }
+    it { should have_many_attached(:photos) }
+    it { should have_many_attached(:files) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :status }
+    it { should validate_presence_of :description }
+    it { should define_enum_for(:status).with_values(%i[good damaged needs_replacement]) }
   end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -10,4 +10,12 @@ RSpec.describe Category, type: :model do
       it { is_expected.to have_db_column(attribute).of_type type }
     end
   end
+
+  describe 'associations' do
+    it { should have_many(:belongings) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :name }
+  end
 end
