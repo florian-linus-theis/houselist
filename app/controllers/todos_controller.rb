37213818f03ea9bc5eaf6697db1569b450a9e73f
Isabel Dockemeyer <isabel.dockemeyer@web.de>
2023-09-04 11:47:56 +0200

    Added Create Report

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,10 +4,12 @@ class TodosController < ApplicationController
   before_action :set_todo, only: %i[update destroy]
   def create
     @todo = Todo.new(todo_params)
+    belonging = Belonging.find(params[:belonging_id])
     @todo.user = current_user
-    @todo.belonging = Belonging.find(params[:belonging_id])
-    if @todo.save
-      redirect_to flat_belonging_path(@belonging)
+    @todo.belonging = belonging
+    authorize @todo
+    if @todo.save && belonging.update(status: @todo.belonging_status)
+      redirect_to flat_path(belonging.flat), notice: 'Created report!'
     else
       render 'flats/show', status: :unprocessable_entity
     end
@@ -24,6 +26,6 @@ class TodosController < ApplicationController
   end
 
   def todo_params
-    params.require(:todo).permit(:description, :status, :description, photos: [], files: [])
+    params.require(:todo).permit(:description, :status, :belonging_status, :description, photos: [], files: [])
   end
 end
