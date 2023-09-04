8faf5a8d735d4d60696ce2e632bc7e239d2c5419
Filipa Vukovic <hrgafilipa@gmail.com>
2023-09-04 17:47:21 +0200

    forms almost done

--- app/helpers/belongings_helper.rb
+++ app/helpers/belongings_helper.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 module BelongingsHelper
+  def status_icon(b_class)
+    case b_class.value.to_sym
+    when :good
+      '<i class="fa-solid fa-check icon-good" style="color: #fdfdfd;"></i>'.html_safe
+    when :demadged
+      '<i class="fa-solid fa-hammer icon-repair" style="color: #fdfdfd;"></i>'.html_safe
+    else
+      '<i class="fa-solid fa-exclamation icon-replacement" style="color: #fdfdfd;"></i>'.html_safe
+    end
+  end
 end
